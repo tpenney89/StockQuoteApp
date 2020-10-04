f8183b8cf053ab85215032f72667f2bda8a1cfe5
tpenney89 <45411408+tpenney89@users.noreply.github.com>
2020-10-04 11:52:29 -0400

    Adding src and pom file for week 5 assignment
    All files in zip folder provided from week 5 notes on blackboard (stockquote-week5.zip )

---
+++ src/test/java/com/origamisoftware/teach/advanced/services/DatabaseStockServiceTest.java
@@ -0,0 +1,22 @@
+package com.origamisoftware.teach.advanced.services;
+
+import com.origamisoftware.teach.advanced.model.StockQuote;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Unit tests for the DatabaseStockService
+ */
+public class DatabaseStockServiceTest {
+
+    @Test
+    public void testGetQuote() throws Exception {
+        DatabaseStockService databaseStockService = new DatabaseStockService();
+        String symbol = "APPL";
+        StockQuote stockQuote = databaseStockService.getQuote(symbol);
+        assertNotNull("Verify we can get a stock quote from the db", stockQuote);
+        assertEquals("Make sure the symbols match", symbol, stockQuote.getSymbol());
+    }
+}
